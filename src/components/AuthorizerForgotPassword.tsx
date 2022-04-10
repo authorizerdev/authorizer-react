c4dfffc5f5c377780afd3670ec7de26c43c05d6c
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-04-10 17:30:21 +0530

    chore: bump 0.18.0

--- src/components/AuthorizerForgotPassword.tsx
+++ src/components/AuthorizerForgotPassword.tsx
@@ -16,7 +16,6 @@ import {
 import { isValidEmail } from '../utils/validations';
 import { formatErrorMessage } from '../utils/format';
 import { Message } from './Message';
-import { hasWindow } from '../utils/window';
 
 export const AuthorizerForgotPassword: FC<{
   setView?: (v: Views) => void;
