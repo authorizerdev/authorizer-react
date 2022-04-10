4008f2527abfd62410deb201491495bc1c7a11ea
Lakhan Samani <lakhan.m.samani@gmail.com>
2022-04-10 19:50:27 +0530

    fix: window undefined error for ssr

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,5 @@
+import { hasWindow } from './window';
+
 export const getIntervalDiff = (accessTokenExpiresAt: number): number => {
   const expiresAt = accessTokenExpiresAt * 1000 - 300000;
   const currentDate = new Date();
@@ -8,17 +10,20 @@ export const getIntervalDiff = (accessTokenExpiresAt: number): number => {
 
 export const getCrypto = () => {
   //ie 11.x uses msCrypto
-  return (window.crypto || (window as any).msCrypto) as Crypto;
+  return hasWindow()
+    ? ((window.crypto || (window as any).msCrypto) as Crypto)
+    : null;
 };
 
 export const createRandomString = () => {
   const charset =
     '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_~.';
   let random = '';
-  const randomValues = Array.from(
-    getCrypto().getRandomValues(new Uint8Array(43))
-  );
-  randomValues.forEach((v) => (random += charset[v % charset.length]));
+  const crypto = getCrypto();
+  if (crypto) {
+    const randomValues = Array.from(crypto.getRandomValues(new Uint8Array(43)));
+    randomValues.forEach((v) => (random += charset[v % charset.length]));
+  }
   return random;
 };
 
