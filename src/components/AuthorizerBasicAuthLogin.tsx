166c6bf10492c4b8a45910f5d426cd3220f39d1d
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-11-06 16:33:16 +0530

    fix: basic auth login

--- src/components/AuthorizerBasicAuthLogin.tsx
+++ src/components/AuthorizerBasicAuthLogin.tsx
@@ -122,7 +122,7 @@ export const AuthorizerBasicAuthLogin: FC<{
         <Message type={MessageType.Error} text={error} onClose={onErrorClose} />
       )}
       <>
-        <form onSubmit={(e) => onSubmit(e)}>
+        <form onSubmit={onSubmit}>
           <StyledFormGroup hasError={!!errorData.email}>
             <label className="form-input-label" htmlFor="email">
               <span>* </span>Email
