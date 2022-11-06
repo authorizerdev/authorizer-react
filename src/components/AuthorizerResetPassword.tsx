ee07c05b986d772c340d7a90cb981e1500f5b3b8
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-11-06 21:45:40 +0530

    fix: reset password

--- src/components/AuthorizerResetPassword.tsx
+++ src/components/AuthorizerResetPassword.tsx
@@ -1,43 +1,51 @@
-import React, { FC, useState } from 'react';
-import { Form, Field } from 'react-final-form';
+import React, { FC, useEffect, useState } from 'react';
 
 import { ButtonAppearance, MessageType } from '../constants';
 import { useAuthorizer } from '../contexts/AuthorizerContext';
-import {
-  Input,
-  Label,
-  FieldWrapper,
-  Required,
-  Button,
-  Error,
-  Wrapper,
-} from '../styles';
+import { Button, Wrapper, StyledFormGroup } from '../styles';
 import { formatErrorMessage } from '../utils/format';
 import { Message } from './Message';
 import { getSearchParams } from '../utils/url';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import PasswordStrengthIndicator from './PasswordStrengthIndicator';
-import { hasErrors } from '../utils/validations';
 
 type Props = {
   onReset?: (res: any) => void;
 };
 
+interface InputDataType {
+  password: string | null;
+  confirmPassword: string | null;
+}
+
 export const AuthorizerResetPassword: FC<Props> = ({ onReset }) => {
   const { token, redirect_uri } = getSearchParams();
   const [error, setError] = useState(!token ? `Invalid token` : ``);
   const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState<InputDataType>({
+    password: null,
+    confirmPassword: null,
+  });
+  const [errorData, setErrorData] = useState<InputDataType>({
+    password: null,
+    confirmPassword: null,
+  });
   const { authorizerRef, config } = useAuthorizer();
   const [disableContinueButton, setDisableContinueButton] = useState(false);
 
-  const onSubmit = async (values: Record<string, string>) => {
+  const onInputChange = async (field: string, value: string) => {
+    setFormData({ ...formData, [field]: value });
+  };
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
     setLoading(true);
     try {
       const res = await authorizerRef.resetPassword({
         token,
-        password: values.password,
-        confirm_password: values.confirm_password,
+        password: formData.password || '',
+        confirm_password: formData.confirmPassword || '',
       });
       setLoading(false);
       setError(``);
@@ -57,6 +65,43 @@ export const AuthorizerResetPassword: FC<Props> = ({ onReset }) => {
     setError(``);
   };
 
+  useEffect(() => {
+    if (formData.password === '') {
+      setErrorData({ ...errorData, password: 'Password is required' });
+    } else {
+      setErrorData({ ...errorData, password: null });
+    }
+  }, [formData.password]);
+
+  useEffect(() => {
+    if (formData.confirmPassword === '') {
+      setErrorData({
+        ...errorData,
+        confirmPassword: 'Confirm password is required',
+      });
+    } else {
+      setErrorData({ ...errorData, confirmPassword: null });
+    }
+  }, [formData.confirmPassword]);
+
+  useEffect(() => {
+    if (formData.password && formData.confirmPassword) {
+      if (formData.confirmPassword !== formData.password) {
+        setErrorData({
+          ...errorData,
+          password: `Password and confirm passwords don't match`,
+          confirmPassword: `Password and confirm passwords don't match`,
+        });
+      } else {
+        setErrorData({
+          ...errorData,
+          password: null,
+          confirmPassword: null,
+        });
+      }
+    }
+  }, [formData.password, formData.confirmPassword]);
+
   return (
     <ThemeProvider theme={theme}>
       <Wrapper>
@@ -67,100 +112,65 @@ export const AuthorizerResetPassword: FC<Props> = ({ onReset }) => {
             onClose={onErrorClose}
           />
         )}
-        <Form
-          onSubmit={onSubmit}
-          validate={(values) => {
-            const errors: Record<string, string> = {};
-
-            if (!values.password) {
-              errors.password = 'Password is required';
-            }
-
-            if (!values.confirm_password) {
-              errors.password = 'Confirm password is required';
-            }
-
-            if (
-              values.password &&
-              values.confirm_password &&
-              values.confirm_password !== values.password
-            ) {
-              errors.confirm_password = `Password and confirm passwords don't match`;
-              errors.password = `Password and confirm passwords don't match`;
-            }
-            return errors;
-          }}
-        >
-          {({ handleSubmit, pristine, values, errors }) => (
-            <form onSubmit={handleSubmit} name="authorizer-reset-password-form">
-              <FieldWrapper>
-                <Field name="password">
-                  {({ input, meta }) => (
-                    <>
-                      <div>
-                        <Label>
-                          <Required>*</Required>
-                          Password
-                        </Label>
-                        <Input
-                          {...input}
-                          type="password"
-                          placeholder="*********"
-                          hasError={Boolean(meta.error && meta.touched)}
-                        />
-                        {meta.error && meta.touched && (
-                          <Error>{meta.error}</Error>
-                        )}
-                      </div>
-                    </>
-                  )}
-                </Field>
-              </FieldWrapper>
-              <FieldWrapper>
-                <Field name="confirm_password">
-                  {({ input, meta }) => (
-                    <div>
-                      <Label>
-                        <Required>*</Required>
-                        Confirm Password
-                      </Label>
-                      <Input
-                        {...input}
-                        type="password"
-                        placeholder="*********"
-                        hasError={Boolean(meta.error && meta.touched)}
-                      />
-                      {meta.error && meta.touched && (
-                        <Error>{meta.error}</Error>
-                      )}
-                    </div>
-                  )}
-                </Field>
-              </FieldWrapper>
-              {config.is_strong_password_enabled && (
-                <>
-                  <PasswordStrengthIndicator
-                    value={values.password}
-                    setDisableButton={setDisableContinueButton}
-                  />
-                  <br />
-                </>
-              )}
-              <Button
-                type="submit"
-                disabled={
-                  pristine ||
-                  loading ||
-                  disableContinueButton ||
-                  hasErrors(errors)
-                }
-                appearance={ButtonAppearance.Primary}
-              >
-                {loading ? `Processing ...` : `Continue`}
-              </Button>
-            </form>
+        <form onSubmit={onSubmit} name="authorizer-reset-password-form">
+          <StyledFormGroup hasError={!!errorData.password}>
+            <label className="form-input-label" htmlFor="password">
+              <span>* </span>Password
+            </label>
+            <input
+              name="password"
+              className="form-input-field"
+              placeholder="********"
+              type="password"
+              value={formData.password || ''}
+              onChange={(e) => onInputChange('password', e.target.value)}
+            />
+            {errorData.password && (
+              <div className="form-input-error">{errorData.password}</div>
+            )}
+          </StyledFormGroup>
+          <StyledFormGroup hasError={!!errorData.confirmPassword}>
+            <label className="form-input-label" htmlFor="confirmPassword">
+              <span>* </span>Confirm Password
+            </label>
+            <input
+              name="confirmPassword"
+              className="form-input-field"
+              placeholder="********"
+              type="password"
+              value={formData.confirmPassword || ''}
+              onChange={(e) => onInputChange('confirmPassword', e.target.value)}
+            />
+            {errorData.confirmPassword && (
+              <div className="form-input-error">
+                {errorData.confirmPassword}
+              </div>
+            )}
+          </StyledFormGroup>
+          {config.is_strong_password_enabled && (
+            <>
+              <PasswordStrengthIndicator
+                value={formData.password || ''}
+                setDisableButton={setDisableContinueButton}
+              />
+              <br />
+            </>
           )}
-        </Form>
+          <Button
+            type="submit"
+            disabled={
+              loading ||
+              disableContinueButton ||
+              !!errorData.password ||
+              !!errorData.confirmPassword ||
+              !formData.password ||
+              !formData.confirmPassword
+            }
+            appearance={ButtonAppearance.Primary}
+          >
+            {loading ? `Processing ...` : `Continue`}
+          </Button>
+        </form>
       </Wrapper>
     </ThemeProvider>
   );
