560ec6746e9da222aac7a25aad4ba2fa3a7481d4
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-11-19 10:47:24 +0530

    fix: reset project

--- src/styledComponents/StyledFooter.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react';
-import styles from '../styles/default.css';
-
-const StyledFooter = ({ children }: { children: ReactNode }) => {
-  return <div className={styles['styled-footer']}>{children}</div>;
-};
-
-export default StyledFooter;
--- src/styledComponents/StyledWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react';
-import styles from '../styles/default.css';
-
-const StyledWrapper = ({ children }: { children: ReactNode }) => {
-  return <div className={styles['styled-wrapper']}>{children}</div>;
-};
-
-export default StyledWrapper;
